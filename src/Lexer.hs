939bb699d2a58fda8a4849d45bcbf9bc19a221ef
Kilian Kilger <kkilger@gmail.com>
2014-12-26 07:35:48 +0100

    Add unit tests

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -4,6 +4,7 @@ import Data.Ratio
 import Data.Word
 import Data.Char (isLetter, isSpace, isAlphaNum, isDigit)
 import Control.Monad.State
+import Test.HUnit (Test(..), assertEqual, runTestTT) 
 
 -- | The 'TokenType' describes the different kinds of tokens
 data TokenType =  StartToken        -- ^ Artificial token, corresponding to a lexer state where no token has been lexed 
@@ -25,7 +26,7 @@ data TokenType =  StartToken        -- ^ Artificial token, corresponding to a le
 data Token = Token { tokenType     :: TokenType -- ^ type of the token
                      , tokenString   :: String  -- ^ string representing the token
                      , tokenPosition :: Int     -- ^ position in the input string
-                   }
+                   } deriving (Show, Eq)
 
 -- | The 'LexerError' indicates a failure in the lexing process
 data LexerError =   InvalidToken String     -- ^ Token is invalid, e.g. contains invalid characters 
@@ -102,3 +103,64 @@ matchNumber s pos | length s <= pos        = Nothing
                                                  number = matchNumberDo (drop pos s) 
                                              in return Token { tokenType=NumberToken, tokenString=number, tokenPosition=pos } 
                  | otherwise               = Nothing
+
+
+-- Unit Test section --
+
+testMatchIdentifier1  = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, tokenString="hugo", tokenPosition=0 }) (matchIdentifier "hugo" 0))
+testMatchIdentifier2  = TestCase (assertEqual "ugo" (Just Token { tokenType=IdentifierToken, tokenString="ugo", tokenPosition=1 }) (matchIdentifier "hugo" 1))
+testMatchIdentifier3  = TestCase (assertEqual "go" (Just Token { tokenType=IdentifierToken, tokenString="go", tokenPosition=2 }) (matchIdentifier "hugo" 2))
+testMatchIdentifier4  = TestCase (assertEqual "o" (Just Token { tokenType=IdentifierToken, tokenString="o", tokenPosition=3 }) (matchIdentifier "hugo" 3))
+testMatchIdentifier5  = TestCase (assertEqual "<Nothing>" (Nothing) (matchIdentifier "hugo" 4))
+testMatchIdentifier6  = TestCase (assertEqual "hello" (Just Token { tokenType=IdentifierToken, tokenString="hello", tokenPosition=0 }) (matchIdentifier "hello welt" 0))
+testMatchIdentifier7  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "hello welt" 22))
+testMatchIdentifier8  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "" 0))
+testMatchIdentifier9  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "" 1))
+testMatchIdentifier10 = TestCase (assertEqual "1hugo" (Nothing) (matchIdentifier "1hugo" 0))
+testMatchIdentifier11 = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, tokenString="hugo", tokenPosition=1 }) (matchIdentifier "1hugo" 1))
+testMatchIdentifier12 = TestCase (assertEqual "+hugo" (Nothing) (matchIdentifier "+hugo" 0))
+testMatchIdentifier13 = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, tokenString="hugo", tokenPosition=1 }) (matchIdentifier "+hugo" 1))
+testMatchIdentifier14 = TestCase (assertEqual "+" (Nothing) (matchIdentifier "+" 0))
+testMatchIdentifier15 = TestCase (assertEqual "-" (Nothing) (matchIdentifier "-" 0))
+testMatchIdentifier16 = TestCase (assertEqual "*" (Nothing) (matchIdentifier "*" 0))
+testMatchIdentifier17 = TestCase (assertEqual "/" (Nothing) (matchIdentifier "/" 0))
+testMatchIdentifier18 = TestCase (assertEqual "(" (Nothing) (matchIdentifier "(" 0))
+testMatchIdentifier19 = TestCase (assertEqual ")" (Nothing) (matchIdentifier ")" 0))
+testMatchIdentifier20 = TestCase (assertEqual "{" (Nothing) (matchIdentifier "{" 0))
+testMatchIdentifier21 = TestCase (assertEqual "}" (Nothing) (matchIdentifier "}" 0))
+testMatchIdentifier22 = TestCase (assertEqual "=" (Nothing) (matchIdentifier "=" 0))
+testMatchIdentifier23 = TestCase (assertEqual "." (Nothing) (matchIdentifier "." 0))
+testMatchIdentifier24 = TestCase (assertEqual "0" (Nothing) (matchIdentifier "0" 0))
+testMatchIdentifier25 = TestCase (assertEqual "1" (Nothing) (matchIdentifier "1" 0))
+testMatchIdentifier26 = TestCase (assertEqual "2" (Nothing) (matchIdentifier "2" 0))
+testMatchIdentifier27 = TestCase (assertEqual "9" (Nothing) (matchIdentifier "9" 0))
+
+tests = TestList [
+                    TestLabel "matchIdentifier Test1"   testMatchIdentifier1,
+                    TestLabel "matchIdentifier Test2"   testMatchIdentifier2,
+                    TestLabel "matchIdentifier Test3"   testMatchIdentifier3,
+                    TestLabel "matchIdentifier Test4"   testMatchIdentifier4,
+                    TestLabel "matchIdentifier Test5"   testMatchIdentifier5,
+                    TestLabel "matchIdentifier Test6"   testMatchIdentifier6,
+                    TestLabel "matchIdentifier Test7"   testMatchIdentifier7,
+                    TestLabel "matchIdentifier Test8"   testMatchIdentifier8,
+                    TestLabel "matchIdentifier Test9"   testMatchIdentifier9,
+                    TestLabel "matchIdentifier Test10"  testMatchIdentifier10,
+                    TestLabel "matchIdentifier Test11"  testMatchIdentifier11,
+                    TestLabel "matchIdentifier Test12"  testMatchIdentifier12,
+                    TestLabel "matchIdentifier Test13"  testMatchIdentifier13,
+                    TestLabel "matchIdentifier Test14"  testMatchIdentifier14,
+                    TestLabel "matchIdentifier Test15"  testMatchIdentifier15,
+                    TestLabel "matchIdentifier Test16"  testMatchIdentifier16,
+                    TestLabel "matchIdentifier Test17"  testMatchIdentifier17,
+                    TestLabel "matchIdentifier Test18"  testMatchIdentifier18,
+                    TestLabel "matchIdentifier Test19"  testMatchIdentifier19,
+                    TestLabel "matchIdentifier Test20"  testMatchIdentifier20,
+                    TestLabel "matchIdentifier Test21"  testMatchIdentifier21,
+                    TestLabel "matchIdentifier Test22"  testMatchIdentifier22,
+                    TestLabel "matchIdentifier Test23"  testMatchIdentifier23,
+                    TestLabel "matchIdentifier Test24"  testMatchIdentifier24,
+                    TestLabel "matchIdentifier Test25"  testMatchIdentifier25,
+                    TestLabel "matchIdentifier Test26"  testMatchIdentifier26,
+                    TestLabel "matchIdentifier Test27"  testMatchIdentifier27
+                 ]
