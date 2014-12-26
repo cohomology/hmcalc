d8aea33a78b8ab52bceacae76e9dbe8a5c76f3c6
Kilian Kilger <kkilger@gmail.com>
2014-12-26 21:53:58 +0100

    Further reduce line width

--- src/Lexer.hs
+++ src/Lexer.hs
@@ -121,28 +121,33 @@ matchNumber s pos = positionMatchHelper NumberToken s pos
 -- Unit Test section --
 
 testMatchIdentifier1  = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, tokenLength=4, 
-                                                                   tokenPosition=0 }) (matchIdentifier "hugo" 0))
+                                                                   tokenPosition=0 }) 
+                                   (matchIdentifier "hugo" 0))
 testMatchIdentifier2  = TestCase (assertEqual "ugo" (Just Token { tokenType=IdentifierToken, 
                                                                   tokenLength=3, tokenPosition=1 }) 
-                                                                        (matchIdentifier "hugo" 1))
+                                   (matchIdentifier "hugo" 1))
 testMatchIdentifier3  = TestCase (assertEqual "go" (Just Token { tokenType=IdentifierToken, 
                                                                  tokenLength=2, tokenPosition=2 }) 
-                                                                        (matchIdentifier "hugo" 2))
+                                   (matchIdentifier "hugo" 2))
 testMatchIdentifier4  = TestCase (assertEqual "o" (Just Token { tokenType=IdentifierToken, 
-                                                                tokenLength=1, tokenPosition=3 }) (matchIdentifier "hugo" 3))
-testMatchIdentifier5  = TestCase (assertEqual "<Nothing>" (Nothing) (matchIdentifier "hugo" 4))
+                                                                tokenLength=1, tokenPosition=3 }) 
+                                   (matchIdentifier "hugo" 3))
+testMatchIdentifier5  = TestCase (assertEqual "<Nothing>" (Nothing) 
+                                   (matchIdentifier "hugo" 4))
 testMatchIdentifier6  = TestCase (assertEqual "hello" (Just Token { tokenType=IdentifierToken, 
                                                                     tokenLength=5, tokenPosition=0 }) 
-                                                        (matchIdentifier "hello welt" 0))
+                                   (matchIdentifier "hello welt" 0))
 testMatchIdentifier7  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "hello welt" 22))
 testMatchIdentifier8  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "" 0))
 testMatchIdentifier9  = TestCase (assertEqual "hello" (Nothing) (matchIdentifier "" 1))
 testMatchIdentifier10 = TestCase (assertEqual "1hugo" (Nothing) (matchIdentifier "1hugo" 0))
 testMatchIdentifier11 = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, 
-                                                                   tokenLength=4, tokenPosition=1 }) (matchIdentifier "1hugo" 1))
+                                                                   tokenLength=4, tokenPosition=1 }) 
+                                   (matchIdentifier "1hugo" 1))
 testMatchIdentifier12 = TestCase (assertEqual "+hugo" (Nothing) (matchIdentifier "+hugo" 0))
 testMatchIdentifier13 = TestCase (assertEqual "hugo" (Just Token { tokenType=IdentifierToken, 
-                                                                   tokenLength=4, tokenPosition=1 }) (matchIdentifier "+hugo" 1))
+                                                                   tokenLength=4, tokenPosition=1 }) 
+                                   (matchIdentifier "+hugo" 1))
 testMatchIdentifier14 = TestCase (assertEqual "+" (Nothing) (matchIdentifier "+" 0))
 testMatchIdentifier15 = TestCase (assertEqual "-" (Nothing) (matchIdentifier "-" 0))
 testMatchIdentifier16 = TestCase (assertEqual "*" (Nothing) (matchIdentifier "*" 0))
@@ -158,9 +163,11 @@ testMatchIdentifier25 = TestCase (assertEqual "1" (Nothing) (matchIdentifier "1"
 testMatchIdentifier26 = TestCase (assertEqual "2" (Nothing) (matchIdentifier "2" 0))
 testMatchIdentifier27 = TestCase (assertEqual "9" (Nothing) (matchIdentifier "9" 0))
 testMatchIdentifier28 = TestCase (assertEqual "HUgO22" (Just Token { tokenType=IdentifierToken, tokenLength=6, 
-                                                                     tokenPosition=0 }) (matchIdentifier "HUgO22" 0))
+                                                                     tokenPosition=0 }) 
+                                   (matchIdentifier "HUgO22" 0))
 testMatchIdentifier29 = TestCase (assertEqual "Ü"  (Just Token { tokenType=IdentifierToken, tokenLength=1, 
-                                                                 tokenPosition=0 }) (matchIdentifier "Ü" 0))
+                                                                 tokenPosition=0 }) 
+                                   (matchIdentifier "Ü" 0))
 
 tests = TestList [
                     TestLabel "matchIdentifier Test1"   testMatchIdentifier1,
