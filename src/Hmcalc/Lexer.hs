d50e0eb53bdc951965406cdcf3e4beee6c76cdbb
Kilian Kilger <kkilger@gmail.com>
2015-01-05 21:05:51 +0100

    Add main lexer function

--- src/Hmcalc/Lexer.hs
+++ src/Hmcalc/Lexer.hs
@@ -11,9 +11,10 @@ module Hmcalc.Lexer(
     initializeLexer
   ) where
 
-import Prelude 
-import Data.Char (isLetter, isAlphaNum, isDigit)
+import Data.Char (isLetter, isAlphaNum, isDigit, isSpace)
 import Control.Monad.State
+import Data.Maybe (fromJust, isJust)
+import Data.List (find)
 
 -- | The 'TokenType' describes the different kinds of tokens
 data TokenType =  StartToken        -- ^ Artificial token, corresponding to a lexer state 
@@ -35,6 +36,7 @@ data TokenType =  StartToken        -- ^ Artificial token, corresponding to a le
                 | AssignmentToken   -- ^ '=' token 
                 | CommaToken        -- ^ ',' token
                 | PowerToken        -- ^ '^' token
+                | TransposeToken    -- ^ `'` token
                 deriving (Show, Eq) 
 
 -- | A 'Token' containes all information about a token, i.e. its literal value, the position in the input string 
@@ -45,12 +47,13 @@ data Token = Token { tokenType       :: TokenType -- ^ type of the token
                    } deriving (Show, Eq)
 
 -- | The 'LexerError' indicates a failure in the lexing process
-data LexerError =  InvalidToken { failurePosition :: Int }  -- ^ Token is invalid, e.g. contains invalid characters 
+data LexerError =  InvalidToken { invalidTokenPosition :: Int }  -- ^ Token is invalid, e.g. contains invalid characters 
 
 -- | The 'LexerInternalState' contains the input string and the current position in the input string 
-data LexerInternalState = LexerPosition { lexerString :: String   -- ^ Input string
-                                          , lexerPosition :: Int  -- ^ Position in input string 
-                                        }
+data LexerInternalState = LexerInternalState { lexerString :: String   -- ^ Input string
+                                               , lexerPosition :: Int  -- ^ Position in input string 
+                                             }
+
 -- | The 'LexerResult' is the result of an invokation of 'lookAhead' or 'getNextToken'
 newtype LexerResult = LexerResult ( Either LexerError Token ) 
 
@@ -63,7 +66,7 @@ initializeLexer :: String -> LexerState
 initializeLexer string = state initialState where
  initialState :: LexerInternalState -> (LexerResult, LexerInternalState)
  initialState _ = (LexerResult ( Right Token { tokenType=StartToken, tokenLength = 0, tokenPosition = 0 } ),  
-   LexerPosition { lexerString=string, lexerPosition=0 } )  
+   LexerInternalState { lexerString=string, lexerPosition=0 } )  
 
 -- | Helper function for the different 'match' routines. It removes as many characters from the string constituting 
 --   the first parameter as the second parameter suggests and invokes the given function (i.e. the third parameter) 
@@ -97,24 +100,26 @@ matchSingleChars s pos | length s <= pos = Nothing
                        | otherwise       = let start = head $ drop pos s  
                                                ret t = return Token { tokenType=t, tokenLength=1, tokenPosition=pos }  
                                            in case start of 
-                                             '+' -> ret PlusToken
-                                             '-' -> ret MinusToken
-                                             '*' -> ret AsteriskToken
-                                             '/' -> ret DivisionToken
-                                             '(' -> ret LeftBracketToken
-                                             ')' -> ret RightBracketToken
-                                             '{' -> ret LeftCurlyToken
-                                             '}' -> ret RightCurlyToken
-                                             ',' -> ret CommaToken
-                                             '=' -> ret AssignmentToken
-                                             '^' -> ret PowerToken
-                                             _   -> Nothing
+                                             '+'  -> ret PlusToken
+                                             '-'  -> ret MinusToken
+                                             '*'  -> ret AsteriskToken
+                                             '/'  -> ret DivisionToken
+                                             '('  -> ret LeftBracketToken
+                                             ')'  -> ret RightBracketToken
+                                             '{'  -> ret LeftCurlyToken
+                                             '}'  -> ret RightCurlyToken
+                                             ','  -> ret CommaToken
+                                             '='  -> ret AssignmentToken
+                                             '^'  -> ret PowerToken
+                                             '\'' -> ret TransposeToken 
+                                             _    -> Nothing
 
 -- | The 'checkNumberStart' function checks, if a given combination of digits forms a number
 --   For example: '0012' is not a valid number, but '0.12' is
 checkNumberStart :: String  -- ^ input string 
                     -> Bool -- ^ is start of valid number
-checkNumberStart s | length s == 1 = isDigit $ head s
+checkNumberStart s | null s        = False
+                   | length s == 1 = isDigit $ head s
                    | otherwise     = isDigit (head s) && not (( head s == '0') && isDigit (head $ tail s)) 
 
 -- | Variant of 'takeWhile' which takes as many elements from a list as boolfunc1 || boolfunc2 is true.
@@ -152,6 +157,36 @@ matchEndToken s pos | pos >= length s = return Token { tokenType=EndToken, token
 
 
 -- | The 'matchFunctions' function returns a list of all token matcher functions
-matchFunctions :: [(String -> Int -> Maybe Token)]
+matchFunctions :: [String -> Int -> Maybe Token]
 matchFunctions = [matchIdentifier, matchSingleChars, matchNumber, matchEndToken]
 
+-- | Increase the position inside the 'LexerInternalState' by the given length
+increasePosition :: LexerInternalState         -- ^ Internal lexer state
+                    -> Int                     -- ^ Increase counter
+                    -> LexerInternalState      -- ^ Resulting state
+increasePosition st ln = LexerInternalState { lexerString = lexerString st,
+                                              lexerPosition = lexerPosition st + ln } 
+
+-- | Skips all whitespace at the current lexer state's position and returns the new state
+skipWhitespace :: LexerInternalState -> LexerInternalState
+skipWhitespace state @ LexerInternalState {lexerString=s, lexerPosition=p}
+                  = let work = drop p s
+                        ws   = length (takeWhile isSpace work)
+                    in increasePosition state ws 
+
+-- | Get the next token  
+getNextToken :: LexerResult -> LexerState
+getNextToken (LexerResult (Left error))  = do oldState <- get
+                                              put oldState
+                                              return (LexerResult (Left error))
+getNextToken (LexerResult (Right _ ))    = do oldState <- get 
+                                              let wsState = skipWhitespace oldState
+                                                  match = find isJust (map (($ (lexerString wsState, 
+                                                                                lexerPosition wsState)) . uncurry) 
+                                                                       matchFunctions)   
+                                              if match == Nothing then do put oldState
+                                                                          return $ LexerResult (Left (InvalidToken 
+                                                                                                 (lexerPosition wsState))) 
+                                              else let token = fromJust $ fromJust match 
+                                                   in do put (increasePosition wsState (tokenLength token)) 
+                                                         return $ LexerResult (Right token) 
